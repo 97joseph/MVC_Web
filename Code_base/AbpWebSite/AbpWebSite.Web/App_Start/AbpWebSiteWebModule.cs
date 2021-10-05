5f936106750b55d2836487da63bdb8c293df9015
josephkibira <33089347+97joseph@users.noreply.github.com>
2021-10-05 12:31:35 +0300

    R

---
+++ Code_base/AbpWebSite/AbpWebSite.Web/App_Start/AbpWebSiteWebModule.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Reflection;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Abp.Cms;
+using Abp.Dependency;
+using Abp.Localization;
+using Abp.Modules;
+using Abp.Startup;
+using AbpWebSite.Web.Localization.AbpWebSite;
+
+namespace AbpWebSite.Web
+{
+    public class AbpWebSiteWebModule : AbpModule
+    {
+        public override Type[] GetDependedModules()
+        {
+            return new[]
+                   {
+                       typeof(AbpWebSiteNHibernateModule),
+                       typeof(AbpWebSiteApplicationModule),
+                       typeof(AbpWebSiteWebApiModule),
+                       typeof(AbpWebSiteApplicationModule),
+                       typeof(AbpCmsNHibernateModule)
+                   };
+        }
+
+        public override void Initialize(IAbpInitializationContext initializationContext)
+        {
+            base.Initialize(initializationContext);
+            IocManager.Instance.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
+
+            LocalizationHelper.RegisterSource<AbpWebSiteLocalizationSource>();
+
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
---
+++ Code_base/AbpWebSite/AbpWebSite.Web/Controllers/DocumentsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AbpWebSite.Web.Controllers
+{
+    public class DocumentsController : AbpWebSiteControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+            //return RedirectToAction("IntroductionWithSimpleTaskSystem");
+        }
+
+        public ActionResult IntroductionWithSimpleTaskSystem()
+        {
+            return View();
+        }
+	}
+}
