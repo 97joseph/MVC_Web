ef154b910e1795b438a9bf375cd6d9029e430bce
josephkibira <33089347+97joseph@users.noreply.github.com>
2021-10-11 17:37:10 +0300

    Code

--- AppCode/Controllers/HomeController.cs
+++ AppCode/Controllers/HomeController.cs
@@ -27,6 +27,18 @@ namespace AppCode.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult EmergingTechnologies()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
